673d2b67f06deeac3490af18b859d97e128333fe
kidman90 <kacprzak90@gmail.com>
2019-02-08 12:34:31 +0100

    03 Make TypeScript Class Usage Safer with Strict Property Initialization

---
+++ 03.ts
@@ -0,0 +1,33 @@
+// class Library {
+//   titles: string[] | undefined;
+//   constructor() { }
+// }
+
+// class Library {
+//   titles: string[];
+//   constructor(underRenovation?: boolean) {
+//     if (!underRenovation) {
+//       this.titles = [
+//         'What if?',
+//         'Flow'
+//       ];
+//     } else {
+//       this.titles = [];
+//     }
+//   }
+// }
+
+class Library {
+  titles!: string[];
+  constructor() { }
+}
+
+
+const library = new Library();
+
+// sometime later & elsewhere in our codebase...
+// if (library.titles) {
+//   const shortTitles = library.titles.filter(title => title.length < 5);
+// }
+
+const shortTitles = library.titles.filter(title => title.length < 5);
--- dist/02.js
+++ dist/02.js
@@ -1,2 +1,17 @@
 // population of africa: 1,287,269,147
 const africaPop = 1287269147;
+class MountInput {
+    constructor() {
+        this.amount = 0;
+    }
+    showTooltip() {
+        // show tooltip
+        setTimeout(() => {
+            // hide tooltip
+        }, 2500);
+    }
+    formatMillion() {
+        return this.amount / 1000000 + 'M';
+    }
+}
+MountInput.MAX_ALLOWED = 99999999;
